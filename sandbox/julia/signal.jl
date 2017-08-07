dae04c822226633944ec6596f2070cc8dbbb2942
Arthur Lui <luiarthur@gmail.com>
2017-08-07 12:49:47 -0700

    yes

--- sandbox/julia/signal.jl
+++ sandbox/julia/signal.jl
@@ -8,10 +8,28 @@ Pkg.build("PyCall")
 Pkg.add("PyPlot")
 =#
 
-using DSP, WAV, PyPlot
+using WAV
 
 # Loading and plotting an audio signal
-s, fs = wavread("/Users/lui9/wav/embraceableYou.wav")
+@time s, fs = wavread("/Users/lui9/wav/embraceableYou.wav");
 
-plot(0:1/fs:(length(s)-1)/fs, s)
-xlabel("Time [s]")
+size(s)
+
+# Plot
+#plot(0:1/fs:(length(s)-1)/fs, s)
+#xlabel("Time [s]")
+
+
+### BLINK GUI in browser
+using Blink
+w = Window() # Open a new window
+body!(w, "Hello World") # Set the body content
+loadurl(w, "http://julialang.org"); # Load a web page
+loadurl(w, "http://luiarthur.github.io"); # Load a web page
+@js w Math.log(10)
+@js w console.log("hello, web-scale world")
+@js w console.log("hello, BLA world")
+bla = "KAJSHDKSAD"
+@js w console.log($(s[1,1]))
+@js w console.log($bla)
+
